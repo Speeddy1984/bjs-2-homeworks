666679386b4609e48a189f16095efed28fe34283
Speeddy1984 <speeddy1984@yandex.ru>
2023-11-27 23:21:07 +0800

    First homework w/codebeauty

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,25 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let discr = b**2 - 4*a*c;
-  
-  if (discr < 0) {
-    arr = [];
-  } else if (discr === 0) {
-      arr.push(-b/(2*a));
-    } else {
-      arr.push((-b + Math.sqrt(discr)) / (2*a));
-      arr.push((-b - Math.sqrt(discr)) / (2*a));
-      }
-  return arr;
+	let arr = [];
+	let discr = b ** 2 - 4 * a * c;
+
+	if (discr < 0) {
+		arr = [];
+	} else if (discr === 0) {
+		arr.push(-b / (2 * a));
+	} else {
+		arr.push((-b + Math.sqrt(discr)) / (2 * a));
+		arr.push((-b - Math.sqrt(discr)) / (2 * a));
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percentPerMonth = percent / 100 / 12;
-  let creditAmount = amount - contribution;
-  let monthlyPayment = creditAmount * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth)**countMonths) - 1)));
-  let totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
-  return totalAmount;
+	let percentPerMonth = percent / 100 / 12;
+	let creditAmount = amount - contribution;
+	let monthlyPayment = creditAmount * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth) ** countMonths) - 1)));
+	let totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
+	return totalAmount;
 }
 
 solveEquation(1, 2, 3);
